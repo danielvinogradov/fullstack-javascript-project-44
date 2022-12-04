ba17648722cfe3ecdded3c67e18faefce0ddda88
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-12-04 12:51:46 +0300

    config: add eslint with airbnb config support

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,15 @@
 import readlineSync from 'readline-sync';
 
 function printWelcomeMessage() {
-    console.log('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
 }
 
 function greetUser() {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 }
 
-export function run() {
-    printWelcomeMessage();
-    greetUser();
+export default function run() {
+  printWelcomeMessage();
+  greetUser();
 }
